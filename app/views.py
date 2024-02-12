ae03db78dd1d3aab6e5ee75cf9688030dcd890a4
Musa Adamu Wakili <musawakiliml@hotmail.com>
2024-02-12 10:22:14 +0100

    Update: changed templates directory

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 
 
 # Create your views here.
 
 def index(request):
    item_list = Item.objects.all()
-   return HttpResponse(item_list)
+   template = loader.get_template('app/index.html')
+
+   context = {
+      "item_list":item_list
+   }
+
+   return HttpResponse(template.render(context, request))
