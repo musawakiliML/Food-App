2b3410cfaac0252d4f014b9c3e3975fc23db17f8
Musa Adamu Wakili <musawakiliml@hotmail.com>
2024-02-12 09:45:09 +0100

    Update: Added templates structure and files

--- food_app/settings.py
+++ food_app/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'food_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIRS = os.path.join(BASE_DIR,"static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
