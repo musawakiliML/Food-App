6accac72743d6755fb3bb30af04c06d3164afa23
Musa Adamu Wakili <musawakiliml@hotmail.com>
2024-02-14 10:53:36 +0100

    Update: styled detail page and index

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
    item_name = models.CharField(max_length=200)
    item_desc = models.CharField(max_length=200)
    item_price = models.IntegerField()
+   item_image = models.CharField(max_length=600, default="https://static.vecteezy.com/system/resources/previews/003/170/825/original/isolated-food-plate-fork-and-spoon-design-free-vector.jpg")
 
    def __str__(self):
        return self.item_name
